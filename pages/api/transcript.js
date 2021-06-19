8492b543a374f7086b34492bebf2e222971a8e07
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2021-06-19 20:45:48 +0530

    transcript done

---
+++ pages/api/transcript.js
@@ -0,0 +1,39 @@
+import { db, storage } from "utils/firebase-admin";
+
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+
+const tmpdir = os.tmpdir();
+const bucket = storage.bucket();
+
+export default async function handler(req, res) {
+  const { userLanguage, meetingId, userId } = req.body;
+  const lang = userLanguage.split("-")[0];
+  const data = [];
+
+  try {
+    const { docs } = await db.collection("meetings").doc(meetingId).collection("messages").get();
+    for (const doc of docs) {
+      const { texts, createdAt, userId: messageUserId } = doc.data();
+      const { text } = texts.filter((item) => item.lang === lang)[0];
+      const date = createdAt.toDate().toLocaleString();
+      data.push(`[${date}] ${messageUserId === userId ? "You" : "Peer"} : ${text}`);
+    }
+
+    const filename = `${meetingId}-${userId}.txt`;
+    const filepath = path.join(tmpdir, filename);
+    const destFilePath = `talkeasy/${filename}`;
+
+    fs.writeFileSync(filepath, data.join("\n"), {});
+    await bucket.upload(filepath, { destination: destFilePath });
+    const file = bucket.file(destFilePath);
+    const [url] = await file.getSignedUrl({ action: "read", expires: "01-01-2500" });
+
+    return res.status(200).json({ url });
+  } catch (e) {
+    console.error(e);
+  }
+
+  return res.status(200).json({ error: true });
+}
--- utils/firebase-admin.js
+++ utils/firebase-admin.js
@@ -1,7 +1,12 @@
 import * as admin from "firebase-admin";
 import serviceAccount from "../firebaseServiceAccount.json";
 
-if (!admin.apps.length) admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+if (!admin.apps.length)
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "talk-easy-d2267.appspot.com",
+  });
 
 export const db = admin.firestore();
 export const firestore = admin.firestore;
+export const storage = admin.storage();
