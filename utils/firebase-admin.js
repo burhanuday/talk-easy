7d1d5e0be0138f9f8a9ce728260846c71168ad6b
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2021-06-19 12:35:58 +0530

    correction to firebase-admin

--- utils/firebase-admin.js
+++ utils/firebase-admin.js
@@ -1,5 +1,5 @@
 import * as admin from "firebase-admin";
-import serviceAccount from "../../../firebaseServiceAccount.json";
+import serviceAccount from "../firebaseServiceAccount.json";
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
